478caaea5191d6a5ffbca3b03e698f8af480f19a
HiroIshida <spitfire.docomo@gmail.com>
2022-06-12 18:15:52 +0900

    Render with material

--- deserialize_and_render.py
+++ deserialize_and_render.py
@@ -6,19 +6,42 @@ sys.path.append(working_dir_path)
 sys.path.extend(["/home/h-ishida/.pyenv/versions/3.10.2/lib/python3.10/site-packages"])
 
 
+import random
+
 import bpy
 import bpycv
 import cv2
 import numpy as np
 
-from randblend.blender_object import BlenderWorld
+from randblend.blender_object import BlenderWorld, FileBasedMaterial
+from randblend.dataset import Dataset
+from randblend.path import get_texture_metainfo_path
 
 if __name__ == "__main__":
     bpycv.clear_all()
     bpy.context.scene.frame_set(1)
     bpy.context.scene.render.engine = "CYCLES"
     bpy.context.scene.cycles.samples = 8
+
+    # randomely pick two materials
+    yaml_path = get_texture_metainfo_path()
+    dataset = Dataset.construct(yaml_path)
+    materials_table_cand = dataset.filter_by_categories(("Wood",))
+    materials_floor_cand = dataset.filter_by_categories(
+        ("Carpet", "WoodFloor", "Asphalt", "Rocks", "Ground", "OfficeCeiling")
+    )
+
+    material_table = random.choice(materials_table_cand)
+    material_floor = random.choice(materials_floor_cand)
+
+    fbmat_wood = FileBasedMaterial.from_ambientcg_id(material_table)
+    fbmat_carpet = FileBasedMaterial.from_ambientcg_id(
+        material_floor, scale=(0.1, 0.1, 0.1)
+    )
+
     bw = BlenderWorld.from_json_file("/tmp/randblend.json")
+    bw["table"].set_material(fbmat_wood)
+    bw["floor"].set_material(fbmat_carpet)
     bw.spawn_all()
 
     camera = bpy.context.scene.camera
