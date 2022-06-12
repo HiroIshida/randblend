0f28493fa9da39f5a728a1356be2515c2f06143a
HiroIshida <spitfire.docomo@gmail.com>
2022-06-13 04:26:55 +0900

    Update shape name table

--- script/create_shape_name_table.py
+++ script/create_shape_name_table.py
@@ -3,7 +3,7 @@ import json
 from randblend.gso_dataset import get_all_gso_names
 from randblend.path import get_gso_dataset_path
 
-flat_object_key_wards = [
+flat_object_include_keywards = [
     "Asus",
     "Snacks",
     "DishTowl",
@@ -40,7 +40,14 @@ flat_object_key_wards = [
     "Super_Mario",
 ]
 
-container_object_key_wards = [
+flat_object_exclude_keywards = [
+    "LEGO_5887_Dino_Defense_HQ",
+    "Cootie_Game",
+    "Melissa_Doug_Chunky_Puzzle_Vehicles",
+    "Hasbro_Dont_Wake_Daddy_Board_Game_NJnjGna4u1a",
+]
+
+container_object_include_keywards = [
     "Frypan",
     "Fry_Pan",
     "Cake_Pan",
@@ -55,6 +62,8 @@ container_object_key_wards = [
     "Ramekin",
     "Dog_Bow",
 ]
+
+container_object_exclude_keywards = []
 
 
 class SpecialGeometry:
@@ -71,6 +80,23 @@ def include_one_of_keywards(gso_name, keywards):
     return False
 
 
+def exclude_all_keywards(gso_name, keywards):
+    for kw in keywards:
+        if kw in gso_name:
+            return False
+    return True
+
+
+def match_keyward_conditions(gso_name, include_keywards, exclude_keywards):
+    if include_keywards is not None:
+        if not include_one_of_keywards(gso_name, include_keywards):
+            return False
+    if exclude_keywards is not None:
+        if not exclude_all_keywards(gso_name, exclude_keywards):
+            return False
+    return True
+
+
 if __name__ == "__main__":
     shape_name_table = {
         "LargeFlatShape": [],
@@ -83,7 +109,9 @@ if __name__ == "__main__":
 
     flat_objects = []
     for gso_name in get_all_gso_names():
-        if not include_one_of_keywards(gso_name, flat_object_key_wards):
+        if not match_keyward_conditions(
+            gso_name, flat_object_include_keywards, flat_object_exclude_keywards
+        ):
             continue
 
         jsonfile_path = gso_dataset_path / gso_name / "data.json"
@@ -92,7 +120,12 @@ if __name__ == "__main__":
             obj = json.load(f)
             bbox_min, bbox_max = obj["kwargs"]["bounds"]
             width_list = [u - l for u, l in zip(bbox_max, bbox_min)]
+
+            min_width = min(width_list)
             max_width = max(width_list)
+
+            if min_width * 4 > max_width:
+                continue
             if max_width > 0.3:
                 shape_name_table["LargeFlatShape"].append(gso_name)
             elif max_width > 0.15:
@@ -101,7 +134,11 @@ if __name__ == "__main__":
                 shape_name_table["SmallFlatShape"].append(gso_name)
 
     for gso_name in get_all_gso_names():
-        if include_one_of_keywards(gso_name, container_object_key_wards):
+        if match_keyward_conditions(
+            gso_name,
+            container_object_include_keywards,
+            container_object_exclude_keywards,
+        ):
             shape_name_table["ContainerShape"].append(gso_name)
 
     shape_name_table_path = gso_dataset_path / "shape_name_table.json"
