bfb1b451fb4af982840fb5afa857aa7f17dd11b3
HiroIshida <spitfire.docomo@gmail.com>
2022-06-12 21:38:08 +0900

    Randomly choose gso object

--- randblend/gso_dataset.py
+++ randblend/gso_dataset.py
@@ -1,9 +1,12 @@
 import json
+import random
+from functools import lru_cache
 from typing import Dict, List
 
 from randblend.path import get_gso_dataset_path
 
 
+@lru_cache(maxsize=None)
 def create_gso_category_table() -> Dict[str, List[str]]:
     table = {}
     for dirpath in get_gso_dataset_path().iterdir():
@@ -21,3 +24,18 @@ def create_gso_category_table() -> Dict[str, List[str]]:
         else:
             table[category].append(object_name)
     return table
+
+
+@lru_cache(maxsize=None)
+def get_all_gso_names() -> List[str]:
+    table = create_gso_category_table()
+    lst = []
+    for val in table.values():
+        lst.extend(val)
+    return lst
+
+
+def randomly_pick_gso_name() -> str:
+    lst = get_all_gso_names()
+    name = random.choice(lst)
+    return name
