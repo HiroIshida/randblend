07378a8a55c0156e9d51023e85df91c89195eba3
HiroIshida <spitfire.docomo@gmail.com>
2022-06-13 13:22:54 +0900

    Put scripts to package scripts

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-blender --background -noaudio --python ./deserialize_and_render.py --render-frame 1
+python3 -m randblend.scripts.pybullet_generate_predicates
+blender --background -noaudio --python ./blender_generate_segmentation.py --render-frame 1
 #blender --background -noaudio --python ./render.py --render-frame 1
